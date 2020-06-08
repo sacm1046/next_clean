a5c705b82eb6f5ecceb52dfff66fb950c8a30a08
Sebastian Contreras <sacm1046@gmail.com>
2020-06-07 22:25:05 -0400

    'title'

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,14 @@
 import { useContext } from 'react'
 import { Context } from '../store/appContext'
+import Head from 'next/head'
 
 const Home = () => {
   const { actions, store } = useContext(Context)
   return (
     <>
+      <Head>
+        <title>Proyecto de Testeo</title>
+      </Head>
       <h1>titulo cambiado</h1>
       <h1>{store.value}</h1>
       <button className="btn btn-primary mr-3" onClick={() => actions.test('chao')}>Change Chao</button>
